b3dfdb4cbbf8500df715f52bd6b94b48b4c6b61b
Alexander Scheid-Rehder <alexsr@uni-koblenz.de>
2017-08-29 17:48:13 +0200

    Update demo to render quad using buffer classes

--- demos/initial_demo/main.cpp
+++ demos/initial_demo/main.cpp
@@ -9,6 +9,9 @@
 #include "vup/Rendering/Trackball_camera.h"
 #include <memory>
 #include <vup/Rendering/V_F_shader_program.h>
+#include <vup/GPU_Storage/VBO.h>
+#include <vup/Rendering/Geometry_primitives.h>
+#include <vup/GPU_Storage/VAO.h>
 
 auto cam = std::make_unique<vup::Trackball_camera>(800, 600);
 
@@ -20,11 +23,18 @@ void resize_callback(GLFWwindow* w, int width, int height) {
 int main() {
     auto window = vup::create_window(800, 600, "Initial Testing", nullptr, nullptr);
     vup::init_GLEW();
+    vup::set_viewport(800, 600);
+    vup::init_demo_OpenGL_params();
     glfwSetWindowSizeCallback(window, resize_callback);
     auto minimal_vertex(std::make_shared<vup::Vertex_shader>("../../src/shader/minimal.vert"));
     auto minimal_fragment(std::make_shared<vup::Fragment_shader>("../../src/shader/minimal.frag"));
     vup::V_F_shader_program minimal(minimal_vertex, minimal_fragment);
+    vup::VAO vao({vup::VBO(vup::Quad().vertices)});
     while (glfwWindowShouldClose(window) == 0) {
+        vup::clear_buffers();
+        minimal.use();
+        vao.bind();
+        glDrawArrays(GL_TRIANGLE_STRIP, 0, 16);
         glfwSwapBuffers(window);
         glfwPollEvents();
     }

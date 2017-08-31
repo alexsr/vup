5b0da2dad5a6fb7eaeaad4b2a34b0e1ff166f664
Alexander Scheid-Rehder <alexsr@uni-koblenz.de>
2017-08-31 17:14:09 +0200

    Add element array buffer class

---
+++ src/libraries/vup/GPU_Storage/Element_buffer.cpp
@@ -0,0 +1,33 @@
+//
+// Alexander Scheid-Rehder
+// alexanderb@scheid-rehder.de
+// https://www.alexsr.de
+// https://github.com/alexsr
+//
+
+#include "Element_buffer.h"
+
+vup::Element_buffer::Element_buffer(GLenum draw_usage)
+        : vup::Buffer(GL_ELEMENT_ARRAY_BUFFER, draw_usage) {
+}
+
+vup::Element_buffer::Element_buffer(const std::vector<unsigned int>& indices,
+                               GLenum draw_usage)
+        : vup::Buffer(GL_ELEMENT_ARRAY_BUFFER, indices, draw_usage),
+          m_count(static_cast<int>(indices.size())){
+}
+
+void vup::Element_buffer::set_data(const std::vector<unsigned int>& data) {
+    Buffer::set_data(data);
+    m_count = static_cast<int>(data.size());
+}
+
+void vup::Element_buffer::set_data(const std::vector<unsigned int>& data,
+                                   GLenum draw_usage) {
+    Buffer::set_data(data, draw_usage);
+    m_count = static_cast<int>(data.size());
+}
+
+int vup::Element_buffer::get_count() const {
+    return m_count;
+}
---
+++ src/libraries/vup/GPU_Storage/Element_buffer.h
@@ -0,0 +1,32 @@
+//
+// Alexander Scheid-Rehder
+// alexanderb@scheid-rehder.de
+// https://www.alexsr.de
+// https://github.com/alexsr
+//
+
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "HidingNonVirtualFunction"
+#ifndef VUP_ELEMENT_BUFFER_H
+#define VUP_ELEMENT_BUFFER_H
+
+#include "Buffer.h"
+
+namespace vup
+{
+    class Element_buffer : public vup::Buffer {
+    public:
+        explicit Element_buffer(GLenum draw_usage = GL_STATIC_DRAW);
+        explicit Element_buffer(const std::vector<unsigned int>& indices,
+                                GLenum draw_usage = GL_STATIC_DRAW);
+        void set_data(const std::vector<unsigned int> &data);
+        void set_data(const std::vector<unsigned int> &data, GLenum draw_usage);
+        int get_count() const;
+    private:
+        int m_count;
+    };
+}
+
+#endif //VUP_ELEMENT_BUFFER_H
+
+#pragma clang diagnostic pop

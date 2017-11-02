90b9a6efa848fcd3738b44d19069c524c238b572
Alexander Scheid-Rehder <alexsr@uni-koblenz.de>
2017-11-02 18:24:12 +0100

    Add update uniform methods

--- src/libraries/vup/Rendering/Shader_program.cpp
+++ src/libraries/vup/Rendering/Shader_program.cpp
@@ -5,6 +5,7 @@
 // https://github.com/alexsr
 //
 
+#include <iostream>
 #include "Shader_program.h"
 
 vup::Shader_program::Shader_program() {
@@ -36,4 +37,85 @@ void vup::Shader_program::link_program() const {
                                  + "Error log: \n"
                                  + error_log};
     }
+}
+
+void vup::Shader_program::update_uniform(const GLchar* name, bool b) {
+    glProgramUniform1i(m_program_id, find_uniform_location(name), b);
+}
+
+void vup::Shader_program::update_uniform(const GLchar* name, int i) {
+    glProgramUniform1i(m_program_id, find_uniform_location(name), i);
+}
+
+void vup::Shader_program::update_uniform(const GLchar* name, float f) {
+    glProgramUniform1f(m_program_id, find_uniform_location(name), f);
+}
+
+void vup::Shader_program::update_uniform(const GLchar* name, double d) {
+    glProgramUniform1d(m_program_id, find_uniform_location(name), d);
+}
+
+void vup::Shader_program::update_uniform(const GLchar* name, glm::vec2 v) {
+    glProgramUniform2fv(m_program_id, find_uniform_location(name), 1, glm::value_ptr(v));
+}
+
+void vup::Shader_program::update_uniform(const GLchar* name, glm::vec3 v) {
+    glProgramUniform3fv(m_program_id, find_uniform_location(name), 1, glm::value_ptr(v));
+}
+
+void vup::Shader_program::update_uniform(const GLchar* name, glm::vec4 v) {
+    glProgramUniform4fv(m_program_id, find_uniform_location(name), 1, glm::value_ptr(v));
+}
+
+void vup::Shader_program::update_uniform(const GLchar* name, glm::ivec2 v) {
+    glProgramUniform2iv(m_program_id, find_uniform_location(name), 1, glm::value_ptr(v));
+}
+
+void vup::Shader_program::update_uniform(const GLchar* name, glm::ivec3 v) {
+    glProgramUniform3iv(m_program_id, find_uniform_location(name), 1, glm::value_ptr(v));
+}
+
+void vup::Shader_program::update_uniform(const GLchar* name, glm::ivec4 v) {
+    glProgramUniform4iv(m_program_id, find_uniform_location(name), 1, glm::value_ptr(v));
+}
+
+void vup::Shader_program::update_uniform(const GLchar* name,
+                                         std::vector<glm::vec2> v) {
+    glProgramUniform2fv(m_program_id, find_uniform_location(name),
+                        static_cast<GLsizei>(v.size()), glm::value_ptr(v.data()[0]));
+}
+
+void vup::Shader_program::update_uniform(const GLchar* name,
+                                         std::vector<glm::vec3> v) {
+    glProgramUniform3fv(m_program_id, find_uniform_location(name),
+                        static_cast<GLsizei>(v.size()), glm::value_ptr(v.data()[0]));
+}
+
+void vup::Shader_program::update_uniform(const GLchar* name,
+                                         std::vector<glm::vec4> v) {
+    glProgramUniform4fv(m_program_id, find_uniform_location(name),
+                        static_cast<GLsizei>(v.size()), glm::value_ptr(v.data()[0]));
+}
+
+void vup::Shader_program::update_uniform(const GLchar* name, glm::mat2 m) {
+    glProgramUniformMatrix2fv(m_program_id, find_uniform_location(name), 1,
+                              GL_FALSE, glm::value_ptr(m));
+}
+
+void vup::Shader_program::update_uniform(const GLchar* name, glm::mat3 m) {
+    glProgramUniformMatrix3fv(m_program_id, find_uniform_location(name), 1,
+                              GL_FALSE, glm::value_ptr(m));
+}
+
+void vup::Shader_program::update_uniform(const GLchar* name, glm::mat4 m) {
+    glProgramUniformMatrix4fv(m_program_id, find_uniform_location(name), 1,
+                              GL_FALSE, glm::value_ptr(m));
+}
+
+GLint vup::Shader_program::find_uniform_location(const GLchar* name) {
+    GLint loc = glGetUniformLocation(m_program_id, name);
+    if (loc == -1) {
+        std::cout << "Uniform not found.\n";
+    }
+    return loc;
 }
--- src/libraries/vup/Rendering/Shader_program.h
+++ src/libraries/vup/Rendering/Shader_program.h
@@ -9,6 +9,7 @@
 #define VUP_SHADER_PROGRAM_H
 
 #include <memory>
+#include <glm/gtc/type_ptr.hpp>
 #include "vup/Rendering/Shader.h"
 
 namespace vup
@@ -17,10 +18,27 @@ namespace vup
     public:
         void use() const;
         virtual void reload() const = 0;
+        void update_uniform(const GLchar* name, bool b);
+        void update_uniform(const GLchar* name, int i);
+        void update_uniform(const GLchar* name, float f);
+        void update_uniform(const GLchar* name, double d);
+        void update_uniform(const GLchar* name, glm::vec2 v);
+        void update_uniform(const GLchar* name, glm::vec3 v);
+        void update_uniform(const GLchar* name, glm::vec4 v);
+        void update_uniform(const GLchar* name, glm::ivec2 v);
+        void update_uniform(const GLchar* name, glm::ivec3 v);
+        void update_uniform(const GLchar* name, glm::ivec4 v);
+        void update_uniform(const GLchar* name, std::vector<glm::vec2> v);
+        void update_uniform(const GLchar* name, std::vector<glm::vec3> v);
+        void update_uniform(const GLchar* name, std::vector<glm::vec4> v);
+        void update_uniform(const GLchar* name, glm::mat2 m);
+        void update_uniform(const GLchar* name, glm::mat3 m);
+        void update_uniform(const GLchar* name, glm::mat4 m);
     protected:
         Shader_program();
         ~Shader_program();
         void link_program() const;
+        GLint find_uniform_location(const GLchar* name);
         virtual void attach_shaders() const = 0;
         virtual void detach_shaders() const = 0;
         GLuint m_program_id;

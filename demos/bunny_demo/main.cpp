alexsr/vup

commit 9d0e7da2369a686e8434416f89b46fe868b76624

Restructure classes Mesh_loader and Mesh

demos/bunny_demo/main.cpp
@@ -35,8 +35,9 @@ int main() {
     auto minimal_fragment(std::make_shared<vup::Fragment_shader>("../../src/shader/normal_rendering.frag"));
     vup::V_F_shader_program minimal(minimal_vertex, minimal_fragment,
                                     vup::gl::Introspection::ubos | vup::gl::Introspection::ssbos);
-    vup::Mesh_loader bunny("../../resources/meshes/bunny.obj");
-    vup::VAO vao(bunny.get_mesh(0));
+    vup::Mesh_loader bunny_loader("../../resources/meshes/bunny.obj");
+    vup::Mesh bunny(bunny_loader.get_mesh(0));
+    vup::VAO vao(bunny);
     vup::OpenGL_debug_logger gl_debug_logger;
     gl_debug_logger.disable_messages(GL_DONT_CARE, GL_DONT_CARE, GL_DEBUG_SEVERITY_NOTIFICATION);
     glm::mat4 model(1.0f);

src/libraries/vup/Geometry/Mesh.cpp
@@ -9,6 +9,7 @@
 
 vup::Mesh::Mesh(const aiMesh* m) {
     m_count = m->mNumVertices;
+    m_faces_count = m->mNumFaces;
     copy_3D_data(m->mVertices, m_count);
     if (m->HasNormals()) {
         copy_3D_data(m->mNormals, m_count);
@@ -34,7 +35,7 @@ vup::Mesh::Mesh(const aiMesh* m) {
     m_index_buffer.set_data(indices);
 }
 
-std::vector<vup::VBO> vup::Mesh::get_VBOs() {
+const std::vector<vup::VBO>& vup::Mesh::get_VBOs() const {
     return m_vbos;
 }
 
@@ -46,7 +47,7 @@ vup::Element_buffer vup::Mesh::get_indices() {
     return m_index_buffer;
 }
 
-unsigned int vup::Mesh::get_count() {
+unsigned int vup::Mesh::get_count() const {
     return m_count;
 }
 

src/libraries/vup/Geometry/Mesh.h
@@ -20,14 +20,15 @@ namespace vup
     class Mesh {
     public:
         explicit Mesh(const aiMesh* m);
-        std::vector<vup::VBO> get_VBOs();
+        const std::vector<vup::VBO>& get_VBOs() const;
         vup::VBO get_VBO(unsigned int i);
         vup::Element_buffer get_indices();
-        unsigned int get_count();
+        unsigned int get_count() const;
     private:
         std::vector<vup::VBO> m_vbos;
         vup::Element_buffer m_index_buffer;
         unsigned int m_count;
+        unsigned int m_faces_count;
         void copy_3D_data(const aiVector3D* data,
                                   unsigned int size);
         void copy_2D_data(const aiVector3D* data, unsigned int size);

src/libraries/vup/Geometry/Mesh_loader.cpp
@@ -20,15 +20,16 @@ vup::Mesh_loader::Mesh_loader(const filesystem::path& path) {
 	if ((scene == nullptr) || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || scene->mNumMeshes <= 0) {
 		throw std::runtime_error{"Mesh file not working. Path: " + path.string() + "\n" + imp.GetErrorString()};
     }
+#pragma omp parallel for
     for (int i = 0; i < scene->mNumMeshes; i++) {
-        m_meshes.emplace_back(scene->mMeshes[i]);
+        m_meshes.at(i) = scene->mMeshes[i];
     }
 }
 
-const std::vector<vup::Mesh>& vup::Mesh_loader::get_meshes() {
+const std::vector<aiMesh*>& vup::Mesh_loader::get_meshes() {
     return m_meshes;
 }
 
-vup::Mesh vup::Mesh_loader::get_mesh(unsigned long i) {
+aiMesh* vup::Mesh_loader::get_mesh(unsigned long i) {
     return m_meshes.at(i);
 }

src/libraries/vup/Geometry/Mesh_loader.h
@@ -21,10 +21,10 @@ namespace vup
     class Mesh_loader {
     public:
         explicit Mesh_loader(const filesystem::path& path);
-        const std::vector<Mesh>& get_meshes();
-        Mesh get_mesh(unsigned long i);
+        const std::vector<aiMesh*>& get_meshes();
+        aiMesh* get_mesh(unsigned long i);
     private:
-        std::vector<Mesh> m_meshes;
+        std::vector<aiMesh*> m_meshes;
     };
 }
 

demos/random_movement_bunny_demo/main.cpp
@@ -40,16 +40,17 @@ int main() {
     auto minimal_fragment(std::make_shared<vup::Fragment_shader>("../../src/shader/normal_rendering.frag"));
     vup::V_F_shader_program minimal(minimal_vertex, minimal_fragment,
                                     vup::gl::Introspection::ubos | vup::gl::Introspection::ssbos);
-    vup::Mesh_loader bunny("../../resources/meshes/bunny.obj");
-    bunny.get_mesh(0).get_VBO(0).bind_base(0);
-    vup::SSBO random_pos(vup::generate_random_data(bunny.get_mesh(0).get_count() * 4, 0, 1.0f), 4);
-    vup::VAO vao(bunny.get_mesh(0));
+    vup::Mesh_loader bunny_loader("../../resources/meshes/bunny.obj");
+    vup::Mesh bunny(bunny_loader.get_mesh(0));
+    vup::VAO vao(bunny);
+    bunny.get_VBO(0).bind_base(0);
+    vup::SSBO random_pos(vup::generate_random_data(bunny.get_count() * 4, 0, 1.0f), 4);
     glm::mat4 model(1.0f);
     MVP mats{model, cam.get_view(), cam.get_projection()};
     bool allow_reset;
     move_verts.update_uniform("dt", 0.0001f);
     auto loop = [&](float dt) {
-        move_verts.run(bunny.get_mesh(0).get_count());
+        move_verts.run(bunny.get_count());
         minimal.use();
         cam.update(window, dt);
         mats.update(model, cam.get_view(), cam.get_projection());
@@ -59,7 +60,7 @@ int main() {
         if (glfwGetKey(window.get(), GLFW_KEY_X) == GLFW_PRESS && allow_reset) {
             move_verts.reload();
             move_verts.update_uniform("dt", 0.0001f);
-            random_pos.update_data(vup::generate_random_data(bunny.get_mesh(0).get_count() * 4, 0, 1.0f));
+            random_pos.update_data(vup::generate_random_data(bunny.get_count() * 4, 0, 1.0f));
             allow_reset = false;
         }
         if (glfwGetKey(window.get(), GLFW_KEY_X) == GLFW_RELEASE) {

cmake/complete_project.cmake
@@ -8,7 +8,6 @@ set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${PROJECT_BINARY_DIR}/bin)
 
 if(CMAKE_CXX_COMPILER_ID MATCHES MSVC)
     add_compile_options("/std:c++latest")
-	add_definitions(-DNODEFAULTLIB:library)
     COPY_ASSIMP_DLL(${DEPENDENCIES_PATH}/lib ${PROJECT_BINARY_DIR}/bin)
 elseif(CMAKE_CXX_COMPILER_ID MATCHES GNU)
     add_compile_options("-std=c++17")

src/libraries/vup/GPU_Storage/Base_VAO.cpp
@@ -34,7 +34,7 @@ vup::Base_VAO::Base_VAO(const vup::Geometric_primitive& primitive,
 }
 
 
-vup::Base_VAO::Base_VAO(vup::Mesh mesh) {
+vup::Base_VAO::Base_VAO(const vup::Mesh& mesh) {
     auto vbos = mesh.get_VBOs();
     glCreateVertexArrays(1, &m_name);
     m_count = mesh.get_count();

commit 72646bbe59ab08923bcac288eaf267b315d2348b

Fix mesh loading bug
Commit 9d0e7da introduced a bug where the aiMesh data seems to be out of memory already. Until a way to prevent this is found, this will be the solution.

demos/bunny_demo/main.cpp
@@ -36,7 +36,7 @@ int main() {
     vup::V_F_shader_program minimal(minimal_vertex, minimal_fragment,
                                     vup::gl::Introspection::ubos | vup::gl::Introspection::ssbos);
     vup::Mesh_loader bunny_loader("../../resources/meshes/bunny.obj");
-    vup::Mesh bunny(bunny_loader.get_mesh(0));
+    vup::Mesh bunny = bunny_loader.get_mesh(0);
     vup::VAO vao(bunny);
     vup::OpenGL_debug_logger gl_debug_logger;
     gl_debug_logger.disable_messages(GL_DONT_CARE, GL_DONT_CARE, GL_DEBUG_SEVERITY_NOTIFICATION);

src/libraries/vup/Geometry/Mesh.cpp
@@ -26,7 +26,7 @@ vup::Mesh::Mesh(const aiMesh* m) {
     }
 
     std::vector<unsigned int> indices;
-    for(unsigned int i = 0; i < m->mNumFaces; i++) {
+    for(unsigned int i = 0; i < m_faces_count; i++) {
         aiFace face = m->mFaces[i];
         for(unsigned int j = 0; j < face.mNumIndices; j++) {
             indices.push_back(face.mIndices[j]);

src/libraries/vup/Geometry/Mesh.h
@@ -10,6 +10,7 @@
 
 #include <vup/GPU_Storage/VBO.h>
 #include <vup/GPU_Storage/Element_buffer.h>
+#include <assimp/scene.h>
 #include <assimp/mesh.h>
 #include <iostream>
 #include <map>

src/libraries/vup/Geometry/Mesh_loader.cpp
@@ -20,17 +20,15 @@ vup::Mesh_loader::Mesh_loader(const filesystem::path& path) {
 	if ((scene == nullptr) || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || scene->mNumMeshes <= 0) {
 		throw std::runtime_error{"Mesh file not working. Path: " + path.string() + "\n" + imp.GetErrorString()};
     }
-    m_meshes.resize(scene->mNumMeshes);
-#pragma omp parallel for
     for (int i = 0; i < scene->mNumMeshes; i++) {
-        m_meshes.at(i) = scene->mMeshes[i];
+        m_meshes.emplace_back(scene->mMeshes[i]);
     }
 }
 
-const std::vector<aiMesh*>& vup::Mesh_loader::get_meshes() {
+const std::vector<vup::Mesh>& vup::Mesh_loader::get_meshes() {
     return m_meshes;
 }
 
-aiMesh* vup::Mesh_loader::get_mesh(unsigned long i) {
+vup::Mesh vup::Mesh_loader::get_mesh(unsigned long i) {
     return m_meshes.at(i);
 }

src/libraries/vup/Geometry/Mesh_loader.h
@@ -21,10 +21,10 @@ namespace vup
     class Mesh_loader {
     public:
         explicit Mesh_loader(const filesystem::path& path);
-        const std::vector<aiMesh*>& get_meshes();
-        aiMesh* get_mesh(unsigned long i);
+        const std::vector<vup::Mesh>& get_meshes();
+        vup::Mesh get_mesh(unsigned long i);
     private:
-        std::vector<aiMesh*> m_meshes;
+        std::vector<vup::Mesh> m_meshes;
     };
 }
 
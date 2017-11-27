2cebb74042b9efae240eecf21cd6a50c704a358c
Alexander Scheid-Rehder <alexsr@uni-koblenz.de>
2017-11-27 16:23:14 +0100

    Update bounding box calculation demo

--- demos/calc_bounding_box/main.cpp
+++ demos/calc_bounding_box/main.cpp
@@ -34,22 +34,96 @@ int main() {
     vup::Trackball_camera cam(width, height);
     vup::print_context_info();
     vup::init_demo_OpenGL_params();
-    auto mesh_random_movement(std::make_shared<vup::Compute_shader>("../../src/shader/compute/calc_bounding_box.comp"));
-    vup::Compute_shader_program move_verts(mesh_random_movement);
+    auto calc_box_shader(std::make_shared<vup::Compute_shader>("../../src/shader/compute/calc_bounding_box.comp"));
+    vup::Compute_shader_program calc_box(calc_box_shader);
     auto minimal_vertex(std::make_shared<vup::Vertex_shader>("../../src/shader/mvp_ubo.vert"));
     auto minimal_fragment(std::make_shared<vup::Fragment_shader>("../../src/shader/normal_rendering.frag"));
     vup::V_F_shader_program minimal(minimal_vertex, minimal_fragment,
                                     vup::gl::Introspection::ubos | vup::gl::Introspection::ssbos);
     vup::Mesh_loader bunny_loader("../../resources/meshes/bunny.obj");
     vup::Mesh bunny(bunny_loader.get_mesh_data(0));
+    std::vector<glm::vec4> bunny_verts = bunny_loader.get_mesh_data(0).vertices;
+    float min_x = FLT_MAX;
+    float min_y = FLT_MAX;
+    float min_z = FLT_MAX;
+    float max_x = -FLT_MAX;
+    float max_y = -FLT_MAX;
+    float max_z = -FLT_MAX;
+    for (auto v : bunny_verts) {
+        if (v.x < min_x) {
+            min_x = v.x;
+        }
+        if (v.y < min_y) {
+            min_y = v.y;
+        }
+        if (v.z < min_z) {
+            min_z = v.z;
+        }
+        if (v.x > max_x) {
+            max_x = v.x;
+        }
+        if (v.y > max_y) {
+            max_y = v.y;
+        }
+        if (v.z > max_z) {
+            max_z = v.z;
+        }
+    }
+    std::cout << "Lower bounds: " << min_x << ", " << min_y << ", " << min_z << "\n";
+    std::cout << "Upper bounds: " << max_x << ", " << max_y << ", " << max_z << "\n";
     vup::VAO vao(bunny);
     bunny.get_VBO(0).bind_base(0);
     glm::mat4 model(1.0f);
     MVP mats{model, cam.get_view(), cam.get_projection()};
     bool allow_reset;
     float delta = 0.001f;
-    move_verts.update_uniform("dt", 0.00001f);
-    move_verts.run(bunny.get_count());
+    int max_blocks = static_cast<int>(glm::ceil(bunny.get_count() / calc_box.get_workgroup_size_x()));
+    calc_box.update_uniform("dt", 0.00001f);
+    calc_box.update_uniform("max_index", bunny.get_count());
+    calc_box.update_uniform("max_blocks", max_blocks);
+    std::cout << "Max blocks " << max_blocks << "\n";
+    struct Bounds {
+        float min_x;
+        float max_x;
+        float min_y;
+        float max_y;
+        float min_z;
+        float max_z;
+    };
+    std::vector<Bounds> bounds(static_cast<unsigned long>(max_blocks));
+    vup::SSBO bounds_ssbo(bounds, 4, vup::gl::Storage::read);
+    glPointSize(5);
+    calc_box.run(bunny.get_count());
+    bounds_ssbo.get_data(bounds);
+    min_x = FLT_MAX;
+    min_y = FLT_MAX;
+    min_z = FLT_MAX;
+    max_x = -FLT_MAX;
+    max_y = -FLT_MAX;
+    max_z = -FLT_MAX;
+    for (auto v : bounds) {
+        if (v.min_x < min_x) {
+            min_x = v.min_x;
+        }
+        if (v.min_y < min_y) {
+            min_y = v.min_y;
+        }
+        if (v.min_z < min_z) {
+            min_z = v.min_z;
+        }
+        if (v.max_x > max_x) {
+            max_x = v.max_x;
+        }
+        if (v.max_y > max_y) {
+            max_y = v.max_y;
+        }
+        if (v.max_z > max_z) {
+            max_z = v.max_z;
+        }
+    }
+    std::cout << "Lower bounds: " << min_x << ", " << min_y << ", " << min_z << "\n";
+    std::cout << "Upper bounds: " << max_x << ", " << max_y << ", " << max_z << "\n";
+
     auto loop = [&](float dt) {
         minimal.use();
         cam.update(window, dt);

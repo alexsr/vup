2d7c9bcfa0d8db8e0236c5518b384ce43dbe9e97
Alexander Scheid-Rehder <alexsr@uni-koblenz.de>
2017-11-30 15:11:06 +0100

    Improve AABB demo speed and data usage
    Add a 2nd compute shader to reduce the results of the first even further.

--- demos/calc_bounding_box/main.cpp
+++ demos/calc_bounding_box/main.cpp
@@ -36,49 +36,23 @@ int main() {
     vup::print_context_info();
     vup::init_demo_OpenGL_params();
     vup::Compute_shader calc_box1024("../../src/shader/compute/calc_bounding_box.comp");
-    vup::V_F_shader minimal("../../src/shader/aabb_move_bunny.vert", "../../src/shader/normal_rendering.frag",
+    vup::Compute_shader calc_box128("../../src/shader/compute/calc_bounding_box128.comp");
+    vup::V_F_shader minimal("../../src/shader/mvp_ubo.vert", "../../src/shader/normal_rendering.frag",
                             vup::gl::Introspection::ubos | vup::gl::Introspection::ssbos);
     vup::Mesh_loader bunny_loader("../../resources/meshes/bunny.obj");
     vup::Mesh bunny(bunny_loader.get_mesh_data(0));
-    std::vector<glm::vec4> bunny_verts = bunny_loader.get_mesh_data(0).vertices;
-    float min_x = FLT_MAX;
-    float min_y = FLT_MAX;
-    float min_z = FLT_MAX;
-    float max_x = -FLT_MAX;
-    float max_y = -FLT_MAX;
-    float max_z = -FLT_MAX;
-    for (auto v : bunny_verts) {
-        if (v.x < min_x) {
-            min_x = v.x;
-        }
-        if (v.y < min_y) {
-            min_y = v.y;
-        }
-        if (v.z < min_z) {
-            min_z = v.z;
-        }
-        if (v.x > max_x) {
-            max_x = v.x;
-        }
-        if (v.y > max_y) {
-            max_y = v.y;
-        }
-        if (v.z > max_z) {
-            max_z = v.z;
-        }
-    }
-    std::cout << "Lower bounds: " << min_x << ", " << min_y << ", " << min_z << "\n";
-    std::cout << "Upper bounds: " << max_x << ", " << max_y << ", " << max_z << "\n";
-    vup::VAO vao(bunny);
-    vup::SSBO current_bunny(bunny_verts, 5);
     glm::mat4 model(1.0f);
-    MVP mats{model, cam.get_view(), cam.get_projection()};
+    MVP mats{glm::mat4(1.0f), cam.get_view(), cam.get_projection()};
     bool allow_reset;
     float delta = 0.001f;
     int max_blocks = static_cast<int>(glm::ceil(bunny.get_count() / calc_box1024.get_workgroup_size_x()));
+    minimal.update_ubo("mvp", mats);
     calc_box1024.update_uniform("max_index", bunny.get_count());
     calc_box1024.update_uniform("max_blocks", max_blocks);
     std::cout << "Max blocks " << max_blocks << "\n";
+    calc_box128.update_uniform("max_index", max_blocks);
+    int max_last_blocks = static_cast<int>(glm::ceil(max_blocks / calc_box128.get_workgroup_size_x()));
+    calc_box128.update_uniform("max_blocks", max_last_blocks);
     struct Bounds {
         float min_x;
         float max_x;
@@ -89,18 +63,19 @@ int main() {
     };
     std::vector<Bounds> bounds(static_cast<unsigned long>(max_blocks));
     vup::SSBO bounds_ssbo(bounds, 4, vup::gl::Storage::read);
+    bunny.get_VBO(0).bind_base(5);
     auto start = std::chrono::system_clock::now();
     calc_box1024.run(bunny.get_count());
-    glFinish();
+    calc_box128.run(max_blocks);
     auto elapsed = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now() - start);
-    std::cout << "Duration for workgroup size " << calc_box1024.get_workgroup_size_x() << ": " << elapsed.count() << "\xE6s\n";
+    bounds.resize(max_last_blocks);
     bounds_ssbo.get_data(bounds);
-    min_x = FLT_MAX;
-    min_y = FLT_MAX;
-    min_z = FLT_MAX;
-    max_x = -FLT_MAX;
-    max_y = -FLT_MAX;
-    max_z = -FLT_MAX;
+    float min_x = FLT_MAX;
+    float min_y = FLT_MAX;
+    float min_z = FLT_MAX;
+    float max_x = -FLT_MAX;
+    float max_y = -FLT_MAX;
+    float max_z = -FLT_MAX;
     for (auto v : bounds) {
         if (v.min_x < min_x) {
             min_x = v.min_x;
@@ -125,29 +100,24 @@ int main() {
     std::cout << "Upper bounds: " << max_x << ", " << max_y << ", " << max_z << "\n";
     std::vector<float> bounds_points{min_x, min_y, min_z, 1.0f, max_x, max_y, max_z, 1.0f};
     glm::vec3 center((min_x + max_x) / 2.0f, (min_y + max_y) / 2.0f, (min_z + max_z) / 2.0f);
-    model = glm::translate(glm::mat4(1.0f), -center);
     vup::VBO bounds_vbo(bounds_points, 4);
     vup::VAO bounds_vao(bounds_vbo);
     vup::V_G_F_shader bounds_renderer("../../src/shader/mvp_ubo_aabb.vert", "../../src/shader/geometry/aabb.geom",
-                                      "../../src/shader/minimal.frag", vup::gl::Introspection::ubos);
+                                      "../../src/shader/minimal.frag");
     elapsed = std::chrono::microseconds(0);
     glm::mat4 rotation(1.0f);
+    vup::VAO vao(bunny);
     float accum = 0;
     auto loop = [&](float dt) {
         accum += 1;
-        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-        minimal.use();
         cam.update(window, dt);
         rotation = glm::rotate(rotation, dt, glm::vec3(0,1,0));
         model = glm::translate(rotation, -center);
         mats.update(model, cam.get_view(), cam.get_projection());
-        bounds_renderer.update_ubo("mvp", mats);
         minimal.update_ubo("mvp", mats);
-        vao.render(GL_TRIANGLES);
-        glFinish();
         start = std::chrono::system_clock::now();
         calc_box1024.run(bunny.get_count());
-        glFinish();
+        calc_box128.run(max_blocks);
         bounds_ssbo.get_data(bounds);
         min_x = FLT_MAX;
         min_y = FLT_MAX;
@@ -155,6 +125,7 @@ int main() {
         max_x = -FLT_MAX;
         max_y = -FLT_MAX;
         max_z = -FLT_MAX;
+#pragma omp parallel for
         for (auto v : bounds) {
             if (v.min_x < min_x) {
                 min_x = v.min_x;
@@ -178,6 +149,9 @@ int main() {
         elapsed += std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now() - start);
         bounds_points = {min_x, min_y, min_z, 1.0f, max_x, max_y, max_z, 1.0f};
         bounds_vbo.update_data(bounds_points);
+        minimal.use();
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+        vao.render(GL_TRIANGLES);
         bounds_renderer.use();
         glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
         bounds_vao.render(GL_LINES);

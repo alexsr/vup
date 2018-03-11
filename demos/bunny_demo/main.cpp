7e45605f97e8328213438eecc70c9bca1e0a71ec
Alexander Scheid-Rehder <alexanderb@scheid-rehder.de>
2018-03-11 16:43:54 +0100

    Fix bunny demo

--- demos/bunny_demo/main.cpp
+++ demos/bunny_demo/main.cpp
@@ -19,23 +19,25 @@ int main() {
     vup::Trackball_camera cam(width, height);
     vup::init_demo_OpenGL_params();
     vup::V_F_shader minimal("../../src/shader/rendering/mvp_ubo.vert",
-                            "../../src/shader/rendering/normal_rendering.frag",
-                            vup::gl::introspection::ubos | vup::gl::introspection::ssbos);
+                            "../../src/shader/rendering/normal_rendering.frag");
     vup::Mesh_loader bunny_loader("../../resources/meshes/bunny.obj");
     const vup::Mesh bunny(bunny_loader.get_mesh_data(0));
     vup::VAO vao(bunny);
     vup::OpenGL_debug_logger gl_debug_logger;
     gl_debug_logger.disable_messages(GL_DONT_CARE, GL_DONT_CARE, GL_DEBUG_SEVERITY_NOTIFICATION);
     vup::MVP mats{glm::mat4(1.0f), cam.get_view(), cam.get_projection()};
+    vup::UBO mvp(mats, 8);
+
     const auto loop = [&](float dt) {
         minimal.use();
         cam.update(window, dt);
         mats.view = cam.get_view();
         mats.projection = cam.get_projection();
-        minimal.update_ubo("mvp", mats);
+        mvp.update_data(mats);
         vao.render(GL_TRIANGLES);
         gl_debug_logger.retrieve_log(std::cout);
     };
+
     window.run_loop_fixed(0.01f, loop);
     glfwTerminate();
     return 0;
